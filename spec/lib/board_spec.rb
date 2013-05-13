2593600ef4ac4e23919b28447ba9a7ddb5237fb6
Brayan Zúñiga <brayan@pernix-solutions.com>
2013-05-12 19:39:06 -0600

    create board specs

---
+++ spec/lib/board_spec.rb
@@ -0,0 +1,13 @@
+require 'board'
+
+describe Board do
+  describe 'play at position' do
+    context 'on an empty board' do
+      it 'plays with a mark on a given position'
+    end
+    context 'on an initialized board' do
+      it 'plays with a mark on a given position'
+      it 'tries to play in a position already played'
+    end
+  end
+end
