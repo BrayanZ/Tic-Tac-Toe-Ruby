db4d5d678b80fbc20cf4ed2d7e8f744a34483cc2
Brayan Zúñiga <brayan@pernix-solutions.com>
2013-05-14 00:20:07 -0600

    passing test for board cells

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,21 @@
 class Board; end
 
 class EmptyBoard
+  def initialize
+    @cells = [EmptyCell.new] * 9
+  end
+
+  def cells
+    @cells
+  end
   def cell_at_position position
-    raise NotImplementedError
+    cells[position]
   end
 
   def make_move(mark: '' , cell:'' )
     cell_at_position(cell).mark_with(mark)
   end
 end
+
+class EmptyCell; end
+
