12a62fb16d5a08a3392daecea3b164e4ef3a1eb2
Brayan Zúñiga <brayan@pernix-solutions.com>
2013-05-15 12:08:00 -0600

    passing plays with a mark on a given position

--- spec/lib/board_spec.rb
+++ spec/lib/board_spec.rb
@@ -15,13 +15,13 @@ describe Board do
     let(:a_cell) {stub :cell, marked?: false}
 
     context 'on an empty board' do
+      let(:board) { EmptyBoard.new }
+
       it 'has 9 empty cells' do
-        board = EmptyBoard.new
         expect(board.cells).to have_only EmptyCell
       end
 
       it 'plays with a mark on a given position' do
-        board = EmptyBoard.new
         board.stub(:cell_at_position).and_return(a_cell)
         a_cell.should_receive(:mark_with).with(A_MARK)
 
@@ -30,13 +30,22 @@ describe Board do
     end
 
     context 'on an initialized board' do
+      let(:a_cell) { stub :cell, marked?: false }
+      let(:a_marked_cell) { stub :cell, marked?: true }
+
       it 'tries to play in a position already played' do
         board = EmptyBoard.new
-        cell = board.cell_at_position(A_CELL_POSITION)
-        cell.stub(:marked?).and_return(true)
+        board.stub(:cell_at_position).with(A_CELL_POSITION).and_return(a_marked_cell)
         expect(board.make_move(mark: A_MARK, cell: A_CELL_POSITION)).to be_nil
       end
-      it 'plays with a mark on a given position'
+
+      it 'plays with a mark on a given position' do
+        board = EmptyBoard.new
+
+        board.stub(:cell_at_position).with(A_CELL_POSITION).and_return(a_cell)
+        a_cell.should_receive(:mark_with).with(A_MARK)
+        board.make_move(mark: A_MARK, cell:A_CELL_POSITION)
+      end
 
     end
   end
