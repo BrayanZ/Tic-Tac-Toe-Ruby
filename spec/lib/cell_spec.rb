6a82d599971819aa7b513c37410490b77c1b14b6
Brayan Zúñiga <brayan@pernix-solutions.com>
2013-05-16 21:38:55 -0600

    Failing cell specs

--- spec/lib/cell_spec.rb
+++ spec/lib/cell_spec.rb
@@ -3,10 +3,22 @@ require 'cell'
 describe Cell do
   A_MARK = :X
   describe 'mark a cell' do
-    it 'marks a cell with the given mark' do
-      cell = described_class.new
-      cell.mark_with A_MARK
-      expect(cell.mark).to eq A_MARK
+    context 'on an empty cell' do
+    let(:empty_cell) { EmptyCell.new }
+      it 'returns an non epty cell when mark' do
+        expect(empty_cell.mark_with(A_MARK)).to be_kind_of Cell
+      end
+      it 'marks a cell with the given mark' do
+        new_cell = empty_cell.mark_with A_MARK
+        expect(new_cell.mark).to eq A_MARK
+      end
+    end
+
+    context 'on a no empty cell' do
+      let(:cell) { Cell.new A_MARK }
+      it "don't mark a already marked" do
+        expect(cell.mark_with A_MARK).to be_nil
+      end
     end
   end
 end
