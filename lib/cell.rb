bf5a2061b1fc4ccfc96dae3548d992206e22b7bd
Brayan Zúñiga <brayan@pernix-solutions.com>
2013-05-16 21:57:55 -0600

    Passig cell specs

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,12 +1,26 @@
 class Cell
-  def mark_with mark
+  def initialize(mark)
     @mark = mark
   end
+
+  def mark_with(_); end
 
   def mark
     @mark
   end
+
+  def marked?
+    raise NotImplementedError
+  end
 end
 
-class EmptyCell; end
+class EmptyCell < Cell
+  def initialize
+    @mark = nil
+  end
+
+  def mark_with(mark)
+    Cell.new(mark)
+  end
+end
 
