55afc8a5073d6a177d4503f564bed94aae8e40dc
Brayan Zúñiga <brayan@pernix-solutions.com>
2013-06-06 19:58:45 -0600

    first definition of the game engine tests

---
+++ spec/lib/game_engine_spec.rb
@@ -0,0 +1,9 @@
+require 'game_engine'
+describe 'game engine' do
+  describe 'start a new game' do
+    it 'creates a new game'
+  end
+  describe 'apply a move' do
+    it 'makes a move on the current board'
+  end
+end
