7bd57ce398331ca3163e6682cd49809859865175
Brayan Zúñiga <brayan@pernix-solutions.com>
2013-06-08 09:02:37 -0600

    failing tests for index page

--- spec/lib/game_engine_spec.rb
+++ spec/lib/game_engine_spec.rb
@@ -1,8 +1,20 @@
+require 'rack/test'
 require 'game_engine'
+
 describe 'game engine' do
-  describe 'start a new game' do
-    it 'creates a new game'
+  include Rack::Test::Methods
+
+  def app
+    GameEngine.new
   end
+
+  describe 'start a new game' do
+    it 'creates a new game' do
+      get('/')
+      expect(last_response.body).to match /<h1.*>Tic-Tac-Toe<\/h1>.*(<a.*data-role='cell'.*>.*){9}/m
+    end
+  end
+
   describe 'apply a move' do
     it 'makes a move on the current board'
   end
