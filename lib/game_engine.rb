7d3e80a67c26ccba673978eb6c3a51247a631b93
Brayan Zúñiga <brayan@pernix-solutions.com>
2013-06-08 11:01:00 -0600

    passing saving session on index

--- lib/game_engine.rb
+++ lib/game_engine.rb
@@ -1,10 +1,12 @@
 require 'sinatra/base'
 
 class GameEngine < Sinatra::Base
+  enable :sessions
   set :views, Proc.new { Dir.pwd + '/views' }
 
   get '/' do
     @game = Game.new 'X', 'O'
+    session[:game] = @game
     erb :index
   end
 end
